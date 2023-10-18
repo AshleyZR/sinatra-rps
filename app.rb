132ab8efb5c0819c0f9f4ecf9f3b0e84f3483eac
Ziyu (Ashley) Ren <92663054+AshleyZR@users.noreply.github.com>
2023-10-18 00:43:21 +0000

    finished rock paper scissors practice

--- app.rb
+++ app.rb
@@ -2,8 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home, { :layout => :homelayout })
 end
+
+get("/rock") do
+  @they = ["r", "p", "s"].sample
+  erb(:rock)
+end
+
+get("/paper") do
+  @they = ["r", "p", "s"].sample
+  erb(:paper)
+end
+
+get("/scissors") do
+  @they = ["r", "p", "s"].sample
+  erb(:scissors)
+end
+
